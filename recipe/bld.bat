39175d17b7cd06d77b20ddc85109e537349926b6
Ray Donnelly <mingw.android@gmail.com>
2017-12-15 05:28:43 -0800

    Attempt to workaround 'fatal error C1002: compiler is out of heap space in pass 2'
    Though I think we may need to update to VS2017 here.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,9 +10,18 @@ cmake -G "Ninja" ^
     -DLLVM_INCLUDE_DOCS=OFF ^
     -DLLVM_ENABLE_RTTI=ON ^
     -DLLVM_INCLUDE_EXAMPLES=OFF ^
+    -DCMAKE_VERBOSE_MAKEFILE=ON ^
     %SRC_DIR%
 
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+:: Building in parallel with VS2015 often fails:
+:: c:\users\builder\m64\conda-bld\llvmdev_1511658421193\work\lib\codegen\globalisel\calllowering.cpp : fatal error C1002: compiler is out of heap space in pass 2
+:: .. even though using a 16GB machine with 16 threads and the 64-bit compilers.
+ninja -j%CPU_COUNT% -v
+:: .. so just rerun from where it fell over.
+ninja -j1 -v
+if errorlevel 1 exit 1
+
+ninja install
 if errorlevel 1 exit 1
