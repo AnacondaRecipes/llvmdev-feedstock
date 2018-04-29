ed608c2af0cc437c64f334256503bd4188f460b0
Ray Donnelly <mingw.android@gmail.com>
2018-04-29 22:09:22 +0100

    Updates from upstream numba/llvmlite

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,27 +1,30 @@
 mkdir build
 cd build
 
-cmake -G "Ninja" ^
-    -DCMAKE_BUILD_TYPE="Release" ^
-    -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-    -DLLVM_INCLUDE_TESTS=OFF ^
-    -DLLVM_INCLUDE_UTILS=OFF ^
-    -DLLVM_INCLUDE_DOCS=OFF ^
-    -DLLVM_ENABLE_RTTI=ON ^
-    -DLLVM_INCLUDE_EXAMPLES=OFF ^
-    -DCMAKE_VERBOSE_MAKEFILE=ON ^
-    %SRC_DIR%
+set BUILD_CONFIG=Release
 
+REM Configure step
+if "%ARCH%"=="32" (
+    set CMAKE_GENERATOR=Visual Studio 14 2015
+) else (
+    set CMAKE_GENERATOR=Visual Studio 14 2015 Win64
+)
+set CMAKE_GENERATOR_TOOLSET=v140_xp
+
+REM Reduce build times and package size by removing unused stuff
+set CMAKE_CUSTOM=-DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_INCLUDE_TESTS=OFF ^
+    -DLLVM_INCLUDE_UTILS=OFF -DLLVM_INCLUDE_DOCS=OFF -DLLVM_INCLUDE_EXAMPLES=OFF ^
+    -DLLVM_ENABLE_ASSERTIONS=ON
+
+cmake -G "%CMAKE_GENERATOR%" -T "%CMAKE_GENERATOR_TOOLSET%" ^
+    -DCMAKE_BUILD_TYPE="%BUILD_CONFIG%" -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% %CMAKE_CUSTOM% %SRC_DIR%
 if errorlevel 1 exit 1
 
-:: Building in parallel with VS2015 often fails:
-:: c:\users\builder\m64\conda-bld\llvmdev_1511658421193\work\lib\codegen\globalisel\calllowering.cpp : fatal error C1002: compiler is out of heap space in pass 2
-:: .. even though using a 16GB machine with 16 threads and the 64-bit compilers.
-ninja -j%CPU_COUNT% -v
-:: .. so just rerun from where it fell over.
-ninja -j1 -v
+REM Build step
+cmake --build . --config "%BUILD_CONFIG%"
 if errorlevel 1 exit 1
 
-ninja install
+REM Install step
+cmake --build . --config "%BUILD_CONFIG%" --target install
 if errorlevel 1 exit 1
